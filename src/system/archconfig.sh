380529c4dd586baeafafd053d889895942a7f469
wmemcpy <celeste.francier@protonmail.com>
2023-12-22 23:52:39 +0100

    Update system configuration and package manager settings

--- src/system/archconfig.sh
+++ src/system/archconfig.sh
@@ -4,10 +4,11 @@ pacman() {
     sudo sed -i 's/^#\(ParallelDownloads.*\)/\1\nILoveCandy/' '/etc/pacman.conf'
     sudo sed -i '/^#\[multilib\]/,/^#Include = \/etc\/pacman.d\/mirrorlist/ s/^#//' '/etc/pacman.conf'
     sudo sed -i 's/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j\$(nproc)\"/' '/etc/makepkg.conf'
-    sudo systemctl enable --now paccache.timer
+    # sudo systemctl enable --now paccache.timer
 }
 
 mirrorlist() {
+    sudo pacman -S --noconfirm --needed reflector
     sudo reflector --verbose --score 100 --latest 20 --fastest 5 --sort rate --save '/etc/pacman.d/mirrorlist'
     sudo pacman -Syyu --noconfirm
 }
@@ -29,17 +30,17 @@ aur() {
     fi
 
     if [[ $aur == "yay" ]]; then
+        sed -i 's/\"sudoloop\": false,/\"sudoloop\": true,/' $HOME/.config/yay/config.json
         yay -Y --gendb
         yay -Y --devel --save
-        sed -i 's/\"sudoloop\": false,/\"sudoloop\": true,/' $HOME/.config/yay/config.json
     elif [[ $aur == "paru" ]]; then
-        paru --gendb
         sudo sed -i 's/#BottomUp/BottomUp/' /etc/paru.conf
         sudo sed -i 's/#SudoLoop/SudoLoop/' /etc/paru.conf
         sudo sed -i 's/#CombinedUpgrade/CombinedUpgrade/' /etc/paru.conf
         sudo sed -i 's/#UpgradeMenu/UpgradeMenu/' /etc/paru.conf
         sudo sed -i 's/#NewsOnUpgrade/NewsOnUpgrade/' /etc/paru.conf
         sudo sh -c 'echo "SkipReview" >> /etc/paru.conf'
+        paru --gendb
     fi
 }
 
